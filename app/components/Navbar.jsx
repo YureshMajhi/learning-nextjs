9e228309707f424e67f594e13de482f47b094282
Yuresh Majhi <yureshmee@gmail.com>
2024-01-28 20:46:31 +0545

    layouts and links

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <Link href="/">dashboard</Link>
+        <Link href="/tickets">tickets</Link>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
